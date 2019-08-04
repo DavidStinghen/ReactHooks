a0d8e3ef6ace2b854b7310bbcb525b3551a6d467
DavidStinghen <davidstinghen@gmail.com>
2019-08-04 14:34:36 -0300

    feature: useState hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,8 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-console': ['error', { allow: ['tron'] }],
-    'no-param-reassign': 'off'
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 };
